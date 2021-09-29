f686f963cf7c99cafce6721cf4d0f7c49f03f20a
audreyboucher <audreyboucher95@gmail.com>
2021-09-29 18:02:43 +0200

    Rearrange folders

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import '../styles/globals.scss';
+import '@styles/globals.scss';
 import type { AppProps } from 'next/app';
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
 };
 
-export default MyApp;
+export default App;
